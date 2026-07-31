1ad084a18f2eb5aee3054a74a20a908a4c8a9f75
Mikasa8051 <renyong_8051@126.com>
2026-07-31 21:08:07 +0800

    Update real_test.py

--- real_test.py
+++ real_test.py
@@ -1,10 +1,19 @@
+import time
+
 from node_converter import build_config
-from xray_runner import start_xray,test_speed,stop_xray
+from xray_runner import start_xray
+from xray_runner import test_speed
+from xray_runner import stop_xray
+
 
 
 def real_test(node):
 
+
+    # 生成Xray配置
+
     config = build_config(node)
+
 
 
     if not config:
@@ -13,16 +22,29 @@ def real_test(node):
 
 
 
-    process=None
+    process = None
+
 
 
     try:
 
 
-        process=start_xray(config)
+        # 启动Xray
+
+        process = start_xray(config)
 
 
-        delay=test_speed()
+
+        # 等待代理启动
+
+        time.sleep(1)
+
+
+
+        # 真实访问测试
+
+        delay = test_speed()
+
 
 
         return delay
@@ -39,6 +61,8 @@ def real_test(node):
     finally:
 
 
+
         if process:
+
 
             stop_xray(process)
