cd913cb27280093cd09b12974fca17eda9ae1175
Mikasa8051 <renyong_8051@126.com>
2026-08-01 12:02:55 +0800

    Update proxy_test.py

--- proxy_test.py
+++ proxy_test.py
@@ -2,65 +2,118 @@ import time
 import requests
 
 
-TEST_URL = "https://www.gstatic.com/generate_204"
+TEST_URLS = [
+
+    "https://www.gstatic.com/generate_204",
+
+    "https://www.cloudflare.com/cdn-cgi/trace",
+
+    "https://raw.githubusercontent.com"
+
+]
 
 
 
 def test_proxy(proxy):
 
-    try:
 
-        start=time.time()
+    success = 0
 
-
-        r=requests.get(
-
-            TEST_URL,
-
-            proxies={
-
-                "http": proxy,
-
-                "https": proxy
-
-            },
-
-            timeout=5
-
-        )
-
-
-        delay=int(
-
-            (time.time()-start)*1000
-
-        )
-
-
-        if r.status_code in [200,204]:
-
-            return {
-
-                "alive":True,
-
-                "delay":delay
-
-            }
-
-
-        return {
-
-            "alive":False
-
-        }
+    delays = []
 
 
 
-    except Exception:
+    proxies = {
+
+        "http": proxy,
+
+        "https": proxy
+
+    }
 
 
-        return {
 
-            "alive":False
+    for url in TEST_URLS:
 
-        }
+
+        try:
+
+
+            start=time.time()
+
+
+
+            r=requests.get(
+
+                url,
+
+                proxies=proxies,
+
+                timeout=8
+
+            )
+
+
+
+            delay=int(
+
+                (time.time()-start)*1000
+
+            )
+
+
+
+            if r.status_code in [200,204]:
+
+
+                success += 1
+
+
+                delays.append(delay)
+
+
+
+        except:
+
+
+            pass
+
+
+
+
+    if success == 0:
+
+
+        return None
+
+
+
+    avg_delay = int(
+
+        sum(delays)
+
+        /
+
+        len(delays)
+
+    )
+
+
+
+    success_rate = round(
+
+        success / len(TEST_URLS),
+
+        2
+
+    )
+
+
+
+    return {
+
+        "delay": avg_delay,
+
+        "success": success_rate
+
+    }
